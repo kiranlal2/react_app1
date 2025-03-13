2c3558e2eda0491fbb4bc49c35be095b0261c9c5
kiranlal2 <kiranlal6dx@gmail.com>
2025-03-13 22:21:18 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
   const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount((current) => {
+      return (current + 1)
+    })
+  }
+  const decrement = () => {
+    setCount((current) => {
+      return (current - 1)
+    })
+  }
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={decrement}>Decrease count</button>
+        <h1>{count}</h1>
+        <button onClick={increment}>Increase count</button>
       </div>
     </>
   )
